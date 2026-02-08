78b78f8baba949e9967ae836fa763c4c5c952700
urotanketto <197749865+urotanketto@users.noreply.github.com>
2026-02-08 19:45:20 +0900

    :wrench: Map CapsLock to Ctrl

--- hosts/xenopus/configuration.nix
+++ hosts/xenopus/configuration.nix
@@ -43,6 +43,8 @@
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.options = "ctrl:nocaps";
+  console.useXkbConfig = true;
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
