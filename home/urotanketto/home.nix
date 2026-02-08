0af470caff01254664db827d6685ded8821fb9cc
urotanketto <197749865+urotanketto@users.noreply.github.com>
2026-02-08 20:12:43 +0900

    :wrench: Disable termguicolors on linux console (TERM=linux)

--- home/urotanketto/home.nix
+++ home/urotanketto/home.nix
@@ -44,7 +44,15 @@
 
     vim.opt.number = true
     vim.opt.signcolumn = "yes"
-    vim.opt.termguicolors = true
+    -- Enable truecolor only on terminals that can handle it well
+    local term = vim.env.TERM or ""
+    local colorterm = vim.env.COLORTERM or ""
+
+    if term ~= "linux" and (colorterm ~= "" or term:find("256color")) then
+      vim.opt.termguicolors = true
+    else
+      vim.opt.termguicolors = false
+    end
 
     vim.opt.expandtab = true
     vim.opt.shiftwidth = 2
