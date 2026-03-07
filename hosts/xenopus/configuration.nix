c887265fd8526f754f80f95370960a092abb7779
urotanketto <197749865+urotanketto@users.noreply.github.com>
2026-03-07 10:46:20 +0900

    :wrench: Add PAM entry for hyprlock

--- hosts/xenopus/configuration.nix
+++ hosts/xenopus/configuration.nix
@@ -142,6 +142,8 @@
     emoji = [ "Noto Color Emoji" ];
   };
 
+  security.pam.services.pyprlock = {};
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
